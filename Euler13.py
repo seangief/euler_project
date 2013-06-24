3fa526ff5e58428bf81e00c6742823b2ca265ed2
Sean Young <sean@Olaf.(none)>
2013-06-24 01:32:08 -0400

    Fixed several of the problems. Please enter the commit message for your changes. Lines starting

--- Euler13.py
+++ Euler13.py
@@ -1,9 +1,11 @@
-euler = open("c:\\euler13.txt","r")
-nums = euler.read()
-euler.close()
-nums = nums.split()
-nums = [number[:12] for number in nums]
-sum=0
-for number in nums:
-	sum += int(number)
-print sum
+def Euler13():
+	nums = open("euler13.txt").read()
+	nums = nums.split()
+	nums = [number[:12] for number in nums]
+	sum=0
+	for number in nums:
+		sum += int(number)
+	print str(sum)[:-4]
+
+if __name__ == "__main__":
+	Euler13()
--- euler12.py
+++ euler12.py
@@ -1,17 +1,13 @@
-def euler12():
-    divs = 0
-    count = 1
-    triangle = (count**2+count)/2
-    
-    while divs < 500:
-        triangle = (count**2+count)/2
-        divs = getNDivs(triangle)
-        count+=1 
-    return triangle
+import isprime as ip
 
-def getNDivs(number):
-   divisors = []
-   for i in range(1,number-1):
-      if number%i == 0:
-         divisors.append(i)
-   return len(divisors)
+def euler12():
+    index, divisors, triangle = 1, 3, 0
+    while divisors < 500:
+        triangle = index*(index+1)/2
+        divisors = len(ip.allfacts(triangle))
+	index+=1
+    print triangle
+
+
+if __name__ == "__main__":
+	euler12()
