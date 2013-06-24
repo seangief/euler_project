a8f0e9c32cde5a6593fce1969d6114970084cd1b
Sean Young <sean@Olaf.(none)>
2013-06-23 22:59:22 -0400

    Fixed Euler 11.

--- Euler11.py
+++ Euler11.py
@@ -1,50 +1,27 @@
 def main():
-    f = open('C:\\Documents and Settings\\Sean Young\\My Documents\\Euler 11.txt','r')
-    data = str(f.read())
-    data = data.split('\n ')
-    matrix = []
-    for line in data:
-            matrix.append(line.split())
-    for h in range(len(matrix)):
-            for k in range(len(matrix[h])):
-                    matrix[h][k] = int(matrix[h][k])
-    multiples = []
-    multiples.extend(downcrawl(matrix))
-    multiples.extend(sidecrawl(matrix))
-    multiples.extend(leftcrawl(matrix))
-    multiples.extend(rightcrawl(matrix))
-    return max(multiples)
+	f = open('Euler11.txt')
+	matrix, ans = [], 0
+	for line in f:
+		matrix.append(line.split())
 
-def downcrawl(matrix):
-   list = []
-   for h in range (len(matrix)):
-      for k in range(len(matrix[h])-3):
-         product = matrix[h][k]*matrix[h][k+1]*matrix[h][k+2]*matrix[h][k+3]
-         list.append(product)
-   return list
+	for h in range(len(matrix)):
+		for k in range(len(matrix[h])):
+			matrix[h][k] = int(matrix[h][k])
 
-def sidecrawl(matrix):
-    list = []
-    for h in range(len(matrix)-3):
-        for k in range(len(matrix[h])):
-            product = matrix[h][k]*matrix[h+1][k]*matrix[h+2][k]*matrix[h+3][k]
-            list.append(product)
-    return list
+	for i in range(len(matrix)):
+		for j in range(len(matrix[i])):
+			a, b = 0,0
+			try:
+				a = reduce(int.__mul__,	[matrix[i+di][j+di] for di in range(4)])
+			except:
+				pass
+			try:
+				b = reduce(int.__mul__,	[matrix[i+di][j-di] for di in range(4)])
+			except:
+				pass
+			if max(a,b) > ans:
+				ans = max(a,b)
+	print ans
 
-def leftcrawl(matrix):
-    list = []
-    for h in range(len(matrix)-3):
-        for k in range(len(matrix[h])-3):
-            product = matrix[h][k]*matrix[h+1][k+1]*matrix[h+2][k+2]*matrix[h+3][k+3]
-            list.append(product)
-    return list
-
-def rightcrawl(matrix):
-    list = []
-    for h in range(len(matrix)-3):
-        for k in range(len(matrix[h])-3):
-            product = matrix[h+3][k]*matrix[h+2][k+1]*matrix[h+1][k+2]*matrix[h][k+3]
-            list.append(product)
-    return list
-
-    
+if __name__ == "__main__":
+	main()
